accc1d564b62e936bff0e4d3179b542c8585bd1b
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2026-04-21 22:53:20 +0300

    Add category

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register
+from .views import register, create_category, category_list
 
 urlpatterns = [
     path('', register, name='register'),
+    path('create-category/', create_category, name='create_category'),
+    path('categories/', category_list, name='category_list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CategoryForm
+from .models import Category
 
 def register(request):
     return render(request, 'users/register.html')
+def create_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    return render(request, 'users/create_category.html', {'form': form})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'users/category_list.html', {'categories': categories})
